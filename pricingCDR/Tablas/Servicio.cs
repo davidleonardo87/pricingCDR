f85c2735e457dbb37df65e8e16b2a190945c0f00
davidleonardo87 <davlerod87@hotmail.com>
2019-06-15 19:17:07 -0500

    create table servicio

---
+++ pricingCDR/Tablas/Servicio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pricingCDR.Tablas
+{
+    public class Servicio
+    {
+        public int IdServicio { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public byte Orden { get; set; }
+    }
+}
